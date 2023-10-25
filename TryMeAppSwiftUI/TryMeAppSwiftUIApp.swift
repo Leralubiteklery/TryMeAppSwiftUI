ac3ebaae6357938031633c3931b6873845d0492d
Lera Savchenko <58272000+Leralubiteklery@users.noreply.github.com>
2023-10-25 19:09:16 +0300

    Initial Commit

---
+++ TryMeAppSwiftUI/TryMeAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TryMeAppSwiftUIApp.swift
+//  TryMeAppSwiftUI
+//
+//  Created by Lera Savchenko on 25.10.23.
+//
+
+import SwiftUI
+
+@main
+struct TryMeAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
