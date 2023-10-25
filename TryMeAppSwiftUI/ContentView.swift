669a9591d8fc908c806b18cef4bcac17ff22cbf8
Lera Savchenko <58272000+Leralubiteklery@users.noreply.github.com>
2023-10-25 20:44:31 +0300

    Created main screen.

--- TryMeAppSwiftUI/ContentView.swift
+++ TryMeAppSwiftUI/ContentView.swift
@@ -8,19 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    var targetValue: Int
+    var currentValue: Int
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Подвиньте слайдер как можно ближe к: \(targetValue)")
+                .padding(EdgeInsets(top: 10, leading: 10, bottom: 16, trailing: 10))
+//            ButtonView(title: "Проверь меня!", action: <#() -> Void#>)
+//            ButtonView(title: "Начать заново", action: <#() -> Int#>)
+                .padding()
         }
-        .padding()
+    }
+}
+
+struct ButtonView: View {
+    let title: String
+    let action: () -> Void
+    
+    var body: some View {
+        Button(title, action: action)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(targetValue: 11, currentValue: 0)
     }
 }
--- TryMeAppSwiftUI/TryMeAppSwiftUIApp.swift
+++ TryMeAppSwiftUI/TryMeAppSwiftUIApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct TryMeAppSwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(targetValue: 11, currentValue: 0)
         }
     }
 }
